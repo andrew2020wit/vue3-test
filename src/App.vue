596d49a384cba8ccd50a7561c618f4aeb5a0c417
andrew2020wit <andrew2020wit@gmail.com>
2021-01-13 21:26:40 +0200

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <h1>App</h1>
+  <input v-model="integer1" />
   <p>integer1: {{ integer1 }}</p>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "App",
   setup() {
     console.log("setup");
-    return {};
+    let integer1 = ref(101);
+    return { integer1 };
   },
   data() {
     return {
-      integer1: 101,
       integer2: 102,
       integer3: 103,
       arr1: [101, 102],
