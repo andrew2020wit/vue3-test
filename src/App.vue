af97028364add1103f00bd7ee750e40f2622ed7d
andrew2020wit <andrew2020wit@gmail.com>
2021-01-13 21:22:02 +0200

    step1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>App</h1>
+  <p>integer1: {{ integer1 }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    console.log("setup");
+    return {};
+  },
+  data() {
+    return {
+      integer1: 101,
+      integer2: 102,
+      integer3: 103,
+      arr1: [101, 102],
+      arr2: [201, 202],
+      arr3: [301, 302],
+      obj1: { a: "a1", b: "b2" },
+    };
+  },
+  methods: {
+    method1(n) {
+      console.log("method1:", this.integer1 * n);
+    },
+  },
+  mounted() {
+    this.method1(2);
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
